0db5f21d3fd61f50fe7537ed928f8be1e03bfd87
Johnathan Driscoll <jdriscoll@cpp.edu>
2022-10-05 19:16:15 -0700

    Initial commit

---
+++ ticTacToe.c
@@ -0,0 +1,14 @@
+//Tic Tac Toe game with pvp or pve
+
+int main() {
+
+    //Prompt user for game mode
+
+    //Read info from console
+
+    //Get player input and check for win loop
+
+    //random computer move
+
+    return 0;
+}
