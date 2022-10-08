e53cc2f104f8ee8ea63edd53fc6ad6dae898d7fa
Johnathan Driscoll <jdriscoll@cpp.edu>
2022-10-07 22:22:24 -0700

    Added player1 move function

--- ticTacToe.c
+++ ticTacToe.c
@@ -13,28 +13,29 @@ const char COMP = 'O';
 void drawBoard();
 void resetBoard();
 int winCheck();
+int checkSpace();
+void player1Move();
+void player2Move();
 
 
 
 
 
 int main() {
-
-    int move;
+    resetBoard();
 
     //Prompt user for game mode
 
+
+
     //Display the board
-    board();
+    drawBoard();
 
 
     //Ask for player move
+    playerMove();
 
-    pritnf("You are X, what is your move?\n");
-    printf("First select a column, left (1), middle (2), or right (3).\n");
-    scanf("%d", &move);
-    position[move] = {"X"};
-    board();
+    
 
     //Create tic tac toe board using print strings
 
@@ -50,13 +51,53 @@ int main() {
     return 0;
 }
 
-void board() {
+void drawBoard() {
 
     printf("+-----------+\n");
-    printf("| %c | %c | %c |\n", position[0], position[1], position[2]);
+    printf("| %c | %c | %c |\n", position[0][0], position[0][1], position[0][2]);
     printf("+-----------+\n");
-    printf("| %c | %c | %c |\n", position[3], position[4], position[5]);
+    printf("| %c | %c | %c |\n", position[1][0], position[1][1], position[1][2]);
     printf("+-----------+\n");
-    printf("| %c | %c | %c |\n", position[6], position[7], position[8]);
+    printf("| %c | %c | %c |\n", position[2][0], position[2][1], position[2][2]);
     printf("+-----------+\n");
     }
+
+void resetBoard(){
+
+}
+
+int winCheck(){
+
+}
+
+int checkSpace(){
+
+}
+
+void playerMove(){
+
+    int row;
+    int col;
+
+    pritnf("You are X, what is your move?\n");
+    printf("First select a column, left (1), middle (2), or right (3).\n");
+    scanf("%d", &col);
+    printf("Now select a row, top (1), middle (2), or bottom (3)"); 
+    scanf("%d", &row);
+    row--;
+    col--;
+    if(position[row][col] != ' ')
+    {
+
+        printf(position[row][col]+" has already moved there.  Pick another space.\n");
+    }
+    else
+    {
+        position[row][col] = PLAYER1;
+    }
+
+}
+void player2Move()
+{
+
+}
