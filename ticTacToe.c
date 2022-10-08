8061d2d3d157f2d0fe5fdba3e2dc7e81ca1b010e
Johnathan Driscoll <jdriscoll@cpp.edu>
2022-10-07 23:33:38 -0700

    added wincheck player1 and player 2 functions

--- ticTacToe.c
+++ ticTacToe.c
@@ -5,7 +5,7 @@
 
 //declaration
 
-char position[3][3];
+char pos[3][3];
 const char PLAYER1 = 'X';
 const char PLAYER2 = 'O';
 const char COMP = 'O';
@@ -16,24 +16,91 @@ int winCheck();
 int checkSpace();
 void player1Move();
 void player2Move();
+void computerMove();
+void gameOver();
+int players;
 
 
-
-
-
-int main() {
+int main() 
+{
     resetBoard();
+
 
     //Prompt user for game mode
 
+    printf("How many players? (1 or 2)\n");
+    scanf("%d", &players);
+    
+    //get moves
+
+    switch(players) {
+        case 1 : //PVP  
+            while(winCheck != 1 && checkSpace !=0){
+        
+                //Display the board
+                drawBoard();
 
 
-    //Display the board
-    drawBoard();
+                //Ask for player move
+                if(winCheck !=1){
+                    player1Move();
+                }
+
+                else{
+                    gameOver();
+                }
+
+                drawBoard();
+
+                if(winCheck !=1){
+                    player2Move();
+                }
+
+                else{
+                    gameOver();
+                }
+            }
+            break;
+
+        case 2 :  //PVE
+            
+             while(winCheck != 1)
+            {
+        
+                //Display the board
+                drawBoard();
 
 
-    //Ask for player move
-    playerMove();
+                //Ask for player move
+                if(winCheck !=1)
+                {
+                    player1Move();
+                }
+
+                else
+                {
+                    gameOver();
+                }
+
+                drawBoard();
+
+                if(winCheck !=1)
+                {
+                    computerMove();
+                }
+
+                else
+                {
+                    gameOver();
+                }
+            }
+
+            break;
+    }
+
+    
+    
+
 
     
 
@@ -54,50 +121,128 @@ int main() {
 void drawBoard() {
 
     printf("+-----------+\n");
-    printf("| %c | %c | %c |\n", position[0][0], position[0][1], position[0][2]);
+    printf("| %c | %c | %c |\n", pos[0][0], pos[0][1], pos[0][2]);
     printf("+-----------+\n");
-    printf("| %c | %c | %c |\n", position[1][0], position[1][1], position[1][2]);
+    printf("| %c | %c | %c |\n", pos[1][0], pos[1][1], pos[1][2]);
     printf("+-----------+\n");
-    printf("| %c | %c | %c |\n", position[2][0], position[2][1], position[2][2]);
+    printf("| %c | %c | %c |\n", pos[2][0], pos[2][1], pos[2][2]);
     printf("+-----------+\n");
     }
 
 void resetBoard(){
+    for(int i = 0; i < 3; i++)
+    {
+        for(int j = 0; j < 3; j++);
+        {
+            pos[i][j] = ' ';
+        }
+    }
+
 
 }
 
 int winCheck(){
 
+    if(pos[0][0] == pos[0][1] && pos[0][1] == pos[0][2] )
+    {
+        return 1;
+    }
+     if(pos[1][0] == pos[1][1] && pos[1][1] == pos[1][2])
+    {
+        return 1;
+    }
+    if(pos[2][0] == pos[2][1] && pos[2][1] == pos[2][2])
+    {
+        return 1;
+    }
+    if(pos[0][0] == pos[1][0] && pos[1][0] == pos[2][0])
+    {
+        return 1;
+    }
+    if(pos[0][1] == pos[1][1] && pos[1][1] == pos[2][1])
+    {
+        return 1;
+    }
+    if(pos[0][2] == pos[1][2] && pos[1][2] == pos[2][2])
+    {
+        return 1;
+    }
+    if(pos[0][0] == pos[1][1] && pos[1][1] == pos[2][2])
+    {
+        return 1;
+    }
+    if(pos[0][2] == pos[1][1] && pos[1][1] == pos[2][0])
+    {
+        return 1;
+    }
 }
 
 int checkSpace(){
+    int movesLeft = 9;
+
+    for(int i = 0; i < 3; i++)
+    {
+        for(int j = 0; j < 3; j++);
+        {
+           if( pos[i][j] =! ' ')
+           {
+                movesLeft--;
+           }
+        }
+    }
+    return movesLeft;
+
 
 }
 
-void playerMove(){
+void player1Move(){
 
     int row;
     int col;
 
     pritnf("You are X, what is your move?\n");
-    printf("First select a column, left (1), middle (2), or right (3).\n");
+    printf("First select a column, left (1), middle (2), or right (3).\n\n");
     scanf("%d", &col);
-    printf("Now select a row, top (1), middle (2), or bottom (3)"); 
+    printf("\nNow select a row, top (1), middle (2), or bottom (3).\n\n"); 
     scanf("%d", &row);
     row--;
     col--;
-    if(position[row][col] != ' ')
+    if(pos[row][col] != ' ')
     {
 
-        printf(position[row][col]+" has already moved there.  Pick another space.\n");
+        printf(pos[row][col]+"\n has already moved there.  Pick another space.\n\n");
     }
     else
     {
-        position[row][col] = PLAYER1;
+        pos[row][col] = PLAYER1;
     }
 
 }
 void player2Move()
+{
+    int row;
+    int col;
+
+    pritnf("You are O, what is your move?\n");
+    printf("First select a column, left (1), middle (2), or right (3).\n\n");
+    scanf("%d", &col);
+    printf("Now select a row, top (1), middle (2), or bottom (3)\n\n"); 
+    scanf("%d", &row);
+    row--;
+    col--;
+    if(pos[row][col] != ' ')
+    {
+
+        printf(pos[row][col]+" has already moved there.  Pick another space.\n\n");
+    }
+    else
+    {
+        pos[row][col] = PLAYER2;
+    }
+
+}
+
+void computerMove()
 {
 
 }
