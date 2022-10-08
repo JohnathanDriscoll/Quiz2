598e9167a161464f221087693e40ba345a1ba1cc
Johnathan Driscoll <jdriscoll@cpp.edu>
2022-10-07 20:38:14 -0700

    create print function

--- ticTacToe.c
+++ ticTacToe.c
@@ -1,24 +1,49 @@
 //Tic Tac Toe game with pvp or pve
+#include <stdio.h>
+#include <stdlib.h>
+
+
+//declarations
+
+char position[10];
+void board();
+int winCheck();
+int top = 1;
+int middle = 2;
+int bottom = 3;
+int left = 1;
+int right = 3;
+
+
+
 
 int main() {
 
     //Prompt user for game mode
+void board();
+    //
 
     //Create tic tac toe board using print strings
 
-    print("+-----------+")
-    print("|   |   |   |")
-    print("+-----------+")
-    print("|   |   |   |")
-    print("+-----------+")
-    print("|   |   |   |")
-    print("+-----------+")
-
+    
     //Read info from console
 
     //Get player input and check for win loop 
 
     //random computer move (not pseudo random) Check code from lecture 9.3
 
+    system("pause");
+
     return 0;
 }
+
+void board() {
+
+    printf("+-----------+");
+    printf("|   |   |   |");
+    printf("+-----------+");
+    printf("|   |   |   |");
+    printf("+-----------+");
+    printf("|   |   |   |");
+    printf("+-----------+");
+    }
